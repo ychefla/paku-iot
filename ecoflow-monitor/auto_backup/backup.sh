48df4fc5a8bc0cc4681e406f3de342eb446ed5d5
Jossu <johannes.kokkila@gmail.com>
2025-11-24 19:47:24 +0000

    chore: archive old services and compose configs into _archive

--- ecoflow-monitor/auto_backup/backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-TIMESTAMP=$(date +"%F")
-BACKUP_DIR="/home/ecoflow/auto_backup/$TIMESTAMP"
-mkdir -p $BACKUP_DIR
-
-docker run --rm --volumes-from prometheus -v $BACKUP_DIR:/backup ubuntu tar cvf /backup/prometheus-backup.tar /prometheus
-docker run --rm --volumes-from grafana -v $BACKUP_DIR:/backup ubuntu tar cvf /backup/grafana-backup.tar /var/lib/grafana
